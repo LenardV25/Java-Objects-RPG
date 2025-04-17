25c5dadcdca0ced930420dec90b36c66521f1c29
LenardV25 <25villal@retsdstudent.ca>
2025-04-17 16:54:58 +0000

    v.0.1

---
+++ player.java
@@ -0,0 +1,16 @@
+public class player{
+    private String name;
+    private int healthPoints;
+    private int attackPower;
+    private int level;
+
+
+    public player(String name, int healthPoints, int attackPower, int level){
+        this.name= name;
+        this.healthPoints = healthPoints;
+        this.attackPower = attackPower;
+        this.level = level;
+        
+    }
+
+}
